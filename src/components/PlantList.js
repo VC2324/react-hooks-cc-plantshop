503f9f650b74d61ad10dfaa9c9908f76e1186763
Victor <victorac2324@gmail.com>
2024-04-10 21:11:13 -0400

    used fetch to display plant images price and names on page deliverable 1 done

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,20 @@
 import React from "react";
 import PlantCard from "./PlantCard";
+import { useState, useEffect } from "react";
 
 function PlantList() {
+const [plants, setPlants] =useState ([])
+useEffect (()=>{
+  fetch (`http://localhost:6001/plants`)
+  .then (response => response.json())
+  .then (plantsArray => setPlants(plantsArray))
+},[])
+ const mappedPlants = plants.map (plant =>{
+  return <PlantCard key ={plant.id} plant ={plant} name={plant.name} price={plant.price} image = {plant.image}/>
+  ///why is it plan and not plants because we use plants as a param for the .map ?
+ })
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{mappedPlants}</ul>
   );
 }
 
